2e651562330c01a7fc65c5c09d106b56cf9a9d57
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-01-05 10:18:17 +0300

    Selection sort in Javascript ............................. The algorithm has a time complexity of O(n2) due to the two loops. The algorithm works by traversing the array and swaping the current index to the smallest number in the array until the whole array is sorted

---
+++ selectionsort.js
@@ -0,0 +1,22 @@
+var arrayToBeSorted ;
+function selectionSort(arrayToBeSorted){
+
+  for(var i =0; i < arrayToBeSorted.length; i++){
+    for(var j =i+ 1; j < arrayToBeSorted.length; j++){
+      if(arrayToBeSorted[i] > arrayToBeSorted[j]){
+         var tempArray = arrayToBeSorted[i];;
+         arrayToBeSorted[i] = arrayToBeSorted[j];
+       arrayToBeSorted[j] = tempArray;
+     }
+
+    }
+     
+  }
+  return arrayToBeSorted;
+
+
+}
+arrayToBeSorted = [23, 4, 50, 7, 45, 99, 22];
+console.log(arrayToBeSorted);
+selectionSort(arrayToBeSorted);
+console.log(arrayToBeSorted);
